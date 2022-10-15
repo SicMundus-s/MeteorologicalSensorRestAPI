2eeb57dfa1341d2b01ab5733e268538458eb87b2
NikitaV <nikita.volkov0505@mail.ru>
2022-10-15 22:12:44 +0300

    Added SQL

--- src/test/java/com/example/meteorologicalsensorrestapi/MeteorologicalSensorRestApiApplicationTests.java
+++ src/test/java/com/example/meteorologicalsensorrestapi/MeteorologicalSensorRestApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.meteorologicalsensorrestapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MeteorologicalSensorRestApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
